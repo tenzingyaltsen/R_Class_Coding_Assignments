81c0900d29d7b6d289970d73ed4b1e81090a9da5
Taran Bhartt <Tarzor101@gmail.com>
2024-07-18 15:07:18 -0400

    Created a working data table, did some preliminary cleaning by converting the dates from character to POSIX. Started to deal with missingness in country data. Created an array that has all of the states matched with their countries, but realized that some states had the same name. Then realized that certain countries were recorded in the city column within brackets, but do not have a corresponding country code

--- Taran_Bhartt_BTC1855H_Assignment_4_Martians.R
+++ Taran_Bhartt_BTC1855H_Assignment_4_Martians.R
@@ -27,13 +27,30 @@
 # Plan
 # 1. Create a data frame using the ufo data
 # 2. look at the data frame to see any obvious issues
-# 3. Create a second array that focusses on the variables 'country', 'shape', 'duration seconds', 'datetime', and 'date_posted'. Edit the titles
+# 3. Create a second array that focusses on the variables 'country', 'shape', 'duration seconds', 'datetime', and 'date_posted'. Edit the titles. Fix data types
 # 4. NUFORC officials have put "((HOAX??))" in the comments. Screen them out
 # 5. Add another column to the dataset called 'report_delay' that caculates the time difference in days between the date of the sighting and the date it was reported.
 # 6. Remove the rows where the sighting was reported before it happened.
 # 7. Create a table with the average report_delay per country.
 # 8. Create a histogram using the 'duration seconds' column.
 
+# NOTE TO REVIEWER
+# install Lubridate
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
 UFOData <- read.csv("ufo_subset.csv")
 
+UFODataClean <- UFOData
 
+UFODataClean$datetime <- strptime(UFODataClean$datetime, format = "%Y-%m-%d %H:%M")
+UFODataClean$date_posted <- strptime(UFODataClean$date_posted, format = "%d-%m-%Y")
+
+table(UFODataClean$country) #identify the list of countries at play. They are all either Australian, Canadian, German, British, or American
+EmptyCountries <- UFODataClean$country=='' #find where the country value is missing
+
+MatchedStateCountry <- unique(UFODataClean[,3:4])
+MatchedStateCountry <- MatchedStateCountry[-which(MatchedStateCountry[,1] == ""|MatchedStateCountry[,2] == ""), ]
+# the result is that the states "dc", "sk", and "wa" are shared across 2 different countries
+# there are countries that are not represented: (france), (venezuala), india, thailand, spain, iran, afghanistan, norway, israel, south korea, honduras, sweden, germany, poland, mexico, japan, china, dominican republic, puerto rico, malaysia, bangladesh, u.a.r., italy, kuwait, romania, ukraine, bulgaria, nepal, bolivia, philippines, bahrain, kosovo, cyprus
